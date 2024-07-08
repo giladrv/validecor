3a39c1d58490e320567fd544a3ece263140aa1ef
giladrv <giladrv@gmail.com>
2024-07-08 21:38:16 +0300

    fully functional decorator

--- core.py
+++ core.py
@@ -6,10 +6,11 @@ from inspect import FullArgSpec, getfullargspec
 from typing import Annotated, Any, Callable, Dict, Type, get_origin
 
 class ErrorType(Enum):
+    ArgCount = 'Unexpected argument count'
     ArgMap = 'Argument map failed'
+    ArgMiss = 'Argument missing'
     ArgVal = 'Argument validation failed'
-    BadAnnotation = 'Invalid annotation metadata'
-    CustomSpec = 'Custom spec does not match actual arguments'
+    BadMetadata = 'Invalid annotation metadata - only Validator instances are allowed'
 
 class ValidecorError(Exception):
     def __init__(self, err_type: ErrorType, **kwargs):
@@ -23,48 +24,16 @@ class ValidecorError(Exception):
     def __str__(self):
         return repr(self)
 
-class Attr(str):
-    def __call__(self, o):
-        return getattr(o, self.name)
-    def __init__(self, name: str):
-        self.name = name
-    def __repr__(self):
-        return f'Attr({repr(self.name)})'
-
-class Map:
-    """
-    Map arguments from a source function spec according to the defined nodes.
-    """
-    def __call__(self, 
-            arg_name: str,
-            arg_type: Type, 
-            arg_map: Dict[str, Any],
-            arg_def: Dict[str, Any]):
-        try:
-            res = arg_map
-            for node in self.nodes:
-                res = node(res) if callable(node) else res[node]
-            return res
-        except Exception as e:
-            if arg_name in arg_def:
-                return arg_def[arg_name]
-            raise ValidecorError(ErrorType.ArgMap, 
-                arg_name = arg_name,
-                arg_type = arg_type,
-                arg_map = self,
-                last_node = node,
-                sub_error = e)
-    def __init__(self, *nodes: int | str | Callable):
-        self.nodes = nodes
-    def __repr__(self):
-        return f'Map({ ",".join(repr(node) for node in self.nodes) })'
-
 class Validator(ABC):
     """
     Base class for argument validators.
     """
     @abstractmethod
-    def __call__(self, arg_name: str, arg_type: Type, arg, arg_map: Dict[str, Any]):
+    def __call__(self, 
+            arg_name: str,
+            arg_type: Type,
+            source_map: Dict[str, Any],
+            target_map: Dict[str, Any]):
         """
         Run-time validation of function arguments.
         
@@ -80,46 +49,113 @@ class Validator(ABC):
     def __repr__(self):
         pass
 
+class Attr(str):
+    def __call__(self, o):
+        return getattr(o, self.name)
+    def __init__(self, name: str):
+        self.name = name
+    def __repr__(self):
+        return f'Attr({repr(self.name)})'
+
+class Map(Validator):
+    """
+    Map arguments from a source function spec according to the defined nodes.
+    """
+    def __call__(self, 
+            arg_name: str,
+            arg_type: Type, 
+            source_map: Dict[str, Any],
+            target_map: Dict[str, Any]):
+        try:
+            res = source_map
+            for node in self.nodes:
+                res = node(res) if callable(node) else res[node]
+            target_map[arg_name] = res
+        except Exception as e:
+            if arg_name not in target_map:
+                raise ValidecorError(ErrorType.ArgMap, 
+                    arg_name = arg_name,
+                    arg_type = arg_type,
+                    arg_map = self,
+                    last_node = node,
+                    sub_error = e)
+    def __init__(self, *nodes: int | str | Callable):
+        self.nodes = nodes
+    def __repr__(self):
+        return f'Map({ ",".join(repr(node) for node in self.nodes) })'
+
+def map_defaults(spec: FullArgSpec):
+    d: Dict[str, Any] = {}
+    if spec.defaults is not None:
+        d.update(zip(spec.args[-len(spec.defaults):], spec.defaults))
+    return d
+
+def map_kwdefaults(spec: FullArgSpec):
+    return spec.kwonlydefaults or {}
+
 def get_arg_map(spec: FullArgSpec, args: tuple, kwargs: Dict[str, Any]):
-    arg_map = kwargs.copy()
-    for arg_name, arg_value in zip(spec.args, args):
-        arg_map[arg_name] = arg_value
-    defaults = get_arg_def(spec)
-    for arg_name, arg_default in defaults.items():
-        if arg_name not in arg_map:
-            arg_map[arg_name] = arg_default
+    arg_map = {}
+    # populate positional arguments
+    spec_defaults = map_defaults(spec)
+    for i, arg_name in enumerate(spec.args):
+        if i < len(args):
+            arg_map[arg_name] = args[i]
+        elif arg_name in spec_defaults:
+            arg_map[arg_name] = spec_defaults[arg_name]
+        else:
+            raise ValidecorError(ErrorType.ArgMiss, arg_name = arg_name)
+    # check for *args
+    if spec.varargs is not None:
+        arg_map[spec.varargs] = args[min(len(args), len(spec.args)):]
+    elif len(args) > len(spec.args):
+        raise ValidecorError(ErrorType.ArgCount, message = 'Too many args')
+    # populate keyword arguments
+    spec_kwdefaults = map_kwdefaults(spec)
+    for arg_name in spec.kwonlyargs:
+        if arg_name in kwargs:
+            arg_map[arg_name] = kwargs[arg_name]
+        elif arg_name in spec_kwdefaults:
+            arg_map[arg_name] = spec_kwdefaults[arg_name]
+        else:
+            raise ValidecorError(ErrorType.ArgMiss, arg_name = arg_name)
+    # check for **kwargs
+    if spec.varkw is not None:
+        arg_map[spec.varkw] = { k: v for k, v in kwargs if k not in spec.kwonlyargs }
+    elif len(kwargs) > len(spec.kwonlyargs):
+        raise ValidecorError(ErrorType.ArgCount, message = 'Too many kwargs')
     return arg_map
 
 def get_arg_def(spec: FullArgSpec):
-    arg_def: Dict[str, Any] = {}
-    if spec.kwonlydefaults is not None:
-        arg_def.update(spec.kwonlydefaults)
-    if spec.defaults is not None:
-        arg_def.update(zip(spec.args[-len(spec.defaults):], spec.defaults))
-    return arg_def
+    return map_defaults(spec) | map_kwdefaults(spec)
 
 def validecor(source_spec: FullArgSpec = None):
     def decorator(fun):
         target_spec = getfullargspec(fun)
         @wraps(fun)
         def wrapper(*args, **kwargs):
-            arg_map = get_arg_map(source_spec or target_spec, args, kwargs)
-            arg_def = get_arg_def(target_spec)
+            source_map = get_arg_map(target_spec if source_spec is None else source_spec, args, kwargs)
+            target_map = source_map if source_spec is None else get_arg_def(target_spec)
+            if target_spec.varargs is not None:
+                target_map[target_spec.varargs] = tuple()
+            if target_spec.varkw is not None:
+                target_map[target_spec.varkw] = {}
             for arg_name, annotation in target_spec.annotations.items():
                 if get_origin(annotation) is Annotated:
                     arg_type = annotation.__origin__
-                    arg_value: Any
-                    arg_orig = True
                     for metadata in annotation.__metadata__:
-                        if isinstance(metadata, Map):
-                            arg_value = metadata(arg_name, arg_type, arg_map, arg_def)
-                            arg_orig = False
-                        elif isinstance(metadata, Validator):
-                            if arg_orig:
-                                arg_value = arg_map[arg_name]
-                            metadata(arg_name, arg_type, arg_value, arg_map)
+                        if isinstance(metadata, Validator):
+                            metadata(arg_name, arg_type, source_map, target_map)
                         else:
-                            raise ValidecorError(ErrorType.BadAnnotation, metadata = metadata)
+                            raise ValidecorError(ErrorType.BadMetadata,
+                                arg_name = arg_name,
+                                metadata = metadata)
+            if source_spec is not None:
+                args = [ target_map[arg_name] for arg_name in target_spec.args ]
+                if target_spec.varargs is not None:
+                    args.extend(target_map[target_spec.varargs])
+                kwargs = { arg_name: target_map[arg_name] for arg_name in target_spec.kwonlyargs }
+                if target_spec.varkw is not None:
+                    kwargs.update(target_map[target_spec.varkw])
             return fun(*args, **kwargs)
         return wrapper
     return decorator
--- validators.py
+++ validators.py
@@ -3,26 +3,6 @@ from typing import Any, Callable, Dict, Type
 # Internal
 from core import ErrorType, ValidecorError, Validator
 
-class IsType(Validator):
-    """
-    Ensure the argument type matches the defined (or override) type.
-    """
-    def __init__(self, type_override: Type = None):
-        self.type_override = type_override
-    def __call__(self, arg_name: str, arg_type: Type, arg, arg_map: dict):
-        actual_type = type(arg)
-        if self.type_override is not None:
-            arg_type = self.type_override
-        if actual_type is not arg_type:
-            raise ValidecorError(ErrorType.ArgVal,
-                arg_name = arg_name,
-                arg_type = arg_type,
-                validator = self)
-    def __repr__(self):
-        name = type(self).__name__
-        details = '' if self.type_override is None else self.type_override.__name__
-        return f'{name}({details})'
-
 class Between(Validator):
     """
     Check that the argument is between lo and hi (inclusive).
@@ -30,8 +10,13 @@ class Between(Validator):
     def __init__(self, lo, hi):
         self.lo = lo
         self.hi = hi
-    def __call__(self, arg_name: str, arg_type: Type, arg, *_):
+    def __call__(self, 
+            arg_name: str,
+            arg_type: Type,
+            _,
+            target_map: Dict[str, Any]):
         try:
+            arg = target_map[arg_name]
             if arg < self.lo or self.hi < arg:
                 raise ValidecorError(ErrorType.ArgVal,
                     arg_name = arg_name,
@@ -56,8 +41,13 @@ class Custom(Validator):
     """
     def __init__(self, validator: Callable[[Any], None]):
         self.validator = validator
-    def __call__(self, arg_name: str, arg_type: Type, arg, *_):
+    def __call__(self, 
+            arg_name: str,
+            arg_type: Type,
+            _,
+            target_map: Dict[str, Any]):
         try:
+            arg = target_map[arg_name]
             self.validator(arg)
         except Exception as e:
             raise ValidecorError(ErrorType.ArgVal,
@@ -70,15 +60,55 @@ class Custom(Validator):
         details = f'{repr(self.validator)}'
         return f'{name}({details})'
 
-class CustomX(Validator):
+class IsType(Validator):
     """
-    Execute a fully custom validator.
+    Ensure the argument type matches the defined (or override) type.
     """
-    def __init__(self, validator: Callable[[str, Type, Any, Dict[str, Any]], None]):
-        self.validator = validator
-    def __call__(self, arg_name: str, arg_type: Type, arg, arg_map: Dict[str, Any]):
-        self.validator(arg_name, arg_type, arg, arg_map)
+    def __init__(self, type_override: Type = None):
+        self.type_override = type_override
+    def __call__(self, 
+            arg_name: str,
+            arg_type: Type,
+            _,
+            target_map: Dict[str, Any]):
+        arg = target_map[arg_name]
+        actual_type = type(arg)
+        if self.type_override is not None:
+            arg_type = self.type_override
+        if actual_type is not arg_type:
+            raise ValidecorError(ErrorType.ArgVal,
+                actual_type = actual_type,
+                arg_name = arg_name,
+                arg_type = arg_type,
+                validator = self)
     def __repr__(self):
         name = type(self).__name__
-        details = f'{repr(self.validator)}'
+        details = '' if self.type_override is None else self.type_override.__name__
+        return f'{name}({details})'
+
+class IsTypable(Validator):
+    """
+    Ensure the argument type matches the defined (or override) type.
+    """
+    def __init__(self, type_override: Type = None):
+        self.type_override = type_override
+    def __call__(self, 
+            arg_name: str,
+            arg_type: Type,
+            _,
+            target_map: Dict[str, Any]):
+        try:
+            arg = target_map[arg_name]
+            if self.type_override is not None:
+                arg_type = self.type_override
+            target_map[arg_name] = arg_type(arg)
+        except Exception as e:
+            raise ValidecorError(ErrorType.ArgVal,
+                arg_name = arg_name,
+                arg_type = arg_type,
+                validator = self,
+                sub_error = e)
+    def __repr__(self):
+        name = type(self).__name__
+        details = '' if self.type_override is None else self.type_override.__name__
         return f'{name}({details})'
